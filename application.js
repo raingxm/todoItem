cf2f8563603109c6e2e9538bd876586f32fba606
raingxm <raingxm@163.com>
2015-06-16 22:58:08 +0800

    add handle submit events

--- application.js
+++ application.js
@@ -4,28 +4,31 @@ $(document).ready(function() {
     });
 
     var TodoForm = Backbone.View.extend({
+        events: {
+          "submit": "handleSubmit"
+        },
         template: _.template('<form>' +
-            '<input name="nn" value="<%= description %>" />' +
-            '<button>Save</button></form>'),
+            '<input name="description" class="col-lg-2" value="<%= description %>" />' +
+            '<button class="col-lg-1">Save</button></form>'),
         render: function() {
             this.$el.html(this.template(this.model.attributes));
             return this;
+        },
+        handleSubmit: function(e) {
+            e.preventDefault();
+            var newDescription = this.$('input[name=description]').val();
+            this.model.save({description: newDescription}, {
+                success: function(model, repsonse, options) {
+                    Backbone.history.navigate("", {trigger: true})
+                },
+                error: function(model, response, options) {
+                    alert('error');
+                }
+            });
         }
     });
 
     var todoItem = new TodoItem({description: 'What do you need todo'});
     var todoForm = new TodoForm({model: todoItem});
     $('#app').html(todoForm.render().el);
-    //var TodoApp = new (Backbone.Router.extend({
-    //    routes: {
-    //        "": "index"
-    //    },
-    //    index: function() {
-    //        $('#app').html('<h1>Hello, Nice day</h1>');
-    //    }
-    //}));
-    //
-    //var AppointmentForm = Backbone.View.extend({
-    //
-    //});
 });
